e0142ce9a6a4c3096a4eed3087b74f4557144a2e
Italis Odilson Woodly <italisodilsonwoodly@hotmail.com>
2023-10-15 21:17:05 -0400

    Implement the method #create_children

--- main.rb
+++ main.rb
@@ -5,4 +5,28 @@ class Square
         @children = []
         @parent = parent
     end
+
+    def create_children
+        potentials = []
+        potentials.push(
+            [board.x + 2, board.y + 1],
+            [board.x + 2, board.y - 1],
+            [board.x + 1, board.y + 2],
+            [board.x + 1, board.y - 2],
+            [board.x - 2, board.y + 1],
+            [board.x - 2, board.y - 1],
+            [board.x - 1, board.y + 2],
+            [board.x - 1, board.y - 2],
+        )
+        valid_children = potentials.select do |space|
+            space[0].between?(0, 8) && space[1].between?(0, 8)
+
+        end
+
+        valid_children = valid_children.map do |space|
+            Square.new(space[0], space[1], board)
+        end
+        
+        @children = valid_children
+    end
 end
